2c65225a3d1ab57153794e307acd78a58d271a2b
Lisky-pixel <o.ndukaaku@alustudent.com>
2026-03-26 03:19:49 +0200

    fix(devops): declare Terraform local provider.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.116.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
